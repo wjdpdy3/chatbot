74b69e123920dc684d10f659ced1b96fa82c9f40
JiHyeon <57008901+wjdpdy3@users.noreply.github.com>
2021-12-29 15:33:02 +0900

    Create MNIST_test.py

---
+++ ex/MNIST_test.py
@@ -0,0 +1,17 @@
+from tensorflow.keras.datasets import mnist
+from tensorflow.keras.models import load_model
+import matplotlib.pyplot as plt
+
+_, (x_test, y_test) = mnist.load_data()
+x_test = x_test / 255.0
+
+model = load_model('mnist_model.h5')
+model.summary()
+model.evaluate(x_test, y_test, verbose=2)
+
+plt.imshow(x_test[20], cmap='gray')
+plt.show()
+
+picks = [20]
+predict = model.predict_classes(x_test[picks])
+print("손글씨 이미지 예측값 : ", predict)
